27e8196f1098bd9772fef38ab0aca552e662b5a1
Daniel <danielezeoke@hotmail.com>
2022-09-09 14:22:56 +0100

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *
+ * main - Entry point
+ *
+ * display set of strings using write instead of printf or puts
+ *
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+
+}
