fd542400467d530bf5f7357da9a6d6b57ac4058b
Daniel <danielezeoke@hotmail.com>
2022-09-09 19:41:45 +0100

    Single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always zero
+ *
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('<n');
+
+	return (0);
+}
