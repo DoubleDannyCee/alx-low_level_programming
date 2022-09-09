12d2417b9247080ba91a5217673c1e913a0fb898
Daniel <danielezeoke@hotmail.com>
2022-09-09 20:03:19 +0100

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - output 0-9 with commas as separations using putchar
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int n ='0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
