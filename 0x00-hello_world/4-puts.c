3957881269b5671f269dd091857a8b38378d8fac
Daniel <danielezeoke@hotmail.com>
2022-09-09 12:02:40 +0100

    Print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+* Program that  prints exactly Programming is like building a multilingual puzzle, followed by a new line
+*
+* Return ; always zero
+*/
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0);
+
+
+}
