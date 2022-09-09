8beb9e41c9f30eb6462e583cd20e8c6d50491b8e
Daniel <danielezeoke@hotmail.com>
2022-09-09 17:50:52 +0100

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always zero
+ *
+ */
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+
+	while(alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	return (0);
+}
