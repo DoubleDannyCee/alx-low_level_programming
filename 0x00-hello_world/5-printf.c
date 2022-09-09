7e6b11e3b99e7deda249f0d35f06e90bfb67b5ab
Daniel <danielezeoke@hotmail.com>
2022-09-09 12:37:00 +0100

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,15 @@
 /**
 *main - Entry point
 *
+*Use the printf function to display a string of characters
+*
 *Return: Always 0 (Success)
 */
 
 int main(void)
 { 
 
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 
