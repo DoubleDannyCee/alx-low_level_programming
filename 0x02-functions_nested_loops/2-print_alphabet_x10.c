e1716bd1aad3fa332d36b2c88cd9e701a1e1e4fe
Daniel <danielezeoke@hotmail.com>
2022-09-13 15:27:21 +0100

    print 10x lowercase alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- function prints lowercase alphabets 10x with new line.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+
+	a = 'a';
+	x = 1;
+
+	while (x <= 11)
+	{
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	x++;
+	}
+	_putchar('\n');
+}
