a912ca211dbcfbd1079eef6e414d2e6658a610df
Daniel <danielezeoke@hotmail.com>
2022-09-13 18:13:54 +0100

    Jack Bauer 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+/*
+ * Doc: 8-24_hours.c
+ * By Daniel Ezeoke
+ */
+
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every min of the day of
+ * 		Jack Bauer, starting from 00:00 to 23:59
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hout = 0; hour <= 23; hour ++)
+	{
+		for (minute = 0; minute <=59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
