f3a0546142b9251afd1662344f8c072b7c905274
Daniel <danielezeoke@hotmail.com>
2022-09-13 18:04:24 +0100

    display last digit number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,12 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
-	_putchar('0' + (n % 10));
+	int l_digit = n % 10;
 
-	return (n % 10);
+	if (l_digit < 0)
+		l_digit *= -1;
+
+	_putchar(l_digit + '0');
+
+	return (l_digit);
 }
