2af6db9c580707695cee7677eb21e6b9dde0a221
Daniel <danielezeoke@hotmail.com>
2022-09-13 18:38:35 +0100

    Sum of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * Doc: 10-add.c
+ * By Daniel Ezeoke
+ */
+
+#include "main.h"
+
+/**
+ * sum - Sums 2 integers
+ * @n1: First integer
+ * @n2: Second integer
+ *
+ * Return: Sum
+ */
+
+int sum(int n1, int n2)
+{
+	return (n1 + n2);
+}
