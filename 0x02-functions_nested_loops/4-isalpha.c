f1749b296e9abf0c43ca450ccbe38f78f2fc351d
Daniel <danielezeoke@hotmail.com>
2022-09-13 16:54:42 +0100

    check for alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabet
+ *
+ * @c: character to check
+ *
+ * Return: 1 is c if letter and 0 otherwise
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c >= '1' && c <= '1')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
