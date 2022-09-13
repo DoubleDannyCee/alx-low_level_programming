b2f6d0ae6ee9a21ed0dadb39d1c54fdbdbe088c2
Daniel <danielezeoke@hotmail.com>
2022-09-13 17:24:39 +0100

    print sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,17 +16,17 @@ int print_sign(int n)
 	if (n > '0')
 	{
 		return (1);
-		putchar('+');
+		_putchar('+');
 	}
 	else if (n == '0')
 	{
 		return (0);
-		putchar('0');
+		_putchar('0');
 	}
 	else
 	{
 		return (-1);
-		putchar('-');
+		_putchar('-');
 	}
 	_putchar('\n');
 }
