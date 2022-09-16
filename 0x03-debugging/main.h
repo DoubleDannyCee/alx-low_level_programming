a0b9ac7434356de6f52b1d8a5479c049ce432c04
Daniel <danielezeoke@hotmail.com>
2022-09-16 02:43:55 +0100

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,7 +7,7 @@ void positive_or_negative(int i);
 
 int main(void);
 
-int largest_number(int, int, int );
+int largest_number(int a, int b, int c);
 
 void print_remaining_days(int month, int day, int year);
 
