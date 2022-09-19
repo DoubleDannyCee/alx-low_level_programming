0568b50cb55ca4e34c14fcf0245b20ab00f64281
Daniel <danielezeoke@hotmail.com>
2022-09-19 18:20:20 +0100

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing prototypes for all functions
+ * 	used in the 0x05-pointers_arrays_strings directory
+ *
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
