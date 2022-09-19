25d1be662a9a7080f84f840cd8e9a1fddc8c72c0
Daniel <danielezeoke@hotmail.com>
2022-09-19 18:26:07 +0100

    Swap values for a and b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * 1-swap: Function to swap the values of two integers
+ *
+ * @a: first value input
+ * @b: Second value input
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *a = b;
+	int *b = a;
+}
