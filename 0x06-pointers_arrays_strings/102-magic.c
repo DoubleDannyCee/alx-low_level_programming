511ebef59d0975b1bfa5c8de3703ac1247bc0c8e
Daniel <danielezeoke@hotmail.com>
2022-09-21 21:44:11 +0100

    Extra Task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
+/**
+ *main - main function
+ *Return: Always 0
+ */
+
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
+	a[2] = 1024;
 
-  p = &n;
+	p = &n;
 
-  *(p + 5) = 98;
+	*(p + 5) = 98;
 
-  printf("a[2] = %d\n", a[2]);
-  
-  return (0);
+	printf("a[2] = %d\n", a[2]);
+
+	return (0);
 }
