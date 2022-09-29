cd0690e8a851dad5f61d51a8b8c86157b1e7029e
DannyCee <danielezeoke@hotmail.com>
2022-09-28 18:49:59 -0700

    recursion task1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  * _puts_recursion - Write a function that prints a string, followed by a new
- *   * line.
- *    *
- *     * @s: This is the input string
- *      */
+ * _puts_recursion - Write a function that prints a string, followed by a new
+ * line.
+ *
+ * @s: This is the input string
+ */
 
 void _puts_recursion(char *s)
 {
