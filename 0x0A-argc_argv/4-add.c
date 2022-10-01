cc46a747dba703141be6ca144bec59d246b2b181
DannyCee <danielezeoke@hotmail.com>
2022-09-30 21:59:23 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,13 +20,15 @@ int main(int argc, char *argv[])
 		{
 			int b;
 			char *str;
-													str = argv[i];
+
+			str = argv[i];
 			for (b = 0; str[b] != '\0'; b++)
 			{
-														if (str[b] < 48 || str[b] > 57)
+
+				if (str[b] < 48 || str[b] > 57)
 				{
-					printf("Error\n");
-					return (1);
+				printf("Error\n");
+				return (1);
 				}
 			}
 		}
