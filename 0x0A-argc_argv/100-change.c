a42277241524bee51035833ee2072b2a75acba3a
DannyCee <danielezeoke@hotmail.com>
2022-09-30 21:56:09 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,7 +5,7 @@
  * main - Prints coints left
  * @args: Paramater Entry
  * @argv: One number of argumets
- * Return: zero onOne number of argumetsr
+ * Return: zero on One number of argumetsr
  */
 
 int main(int args, char *argv[])
@@ -27,7 +27,8 @@ int main(int args, char *argv[])
 	{
 		if (c >= 25)
 			c -= 25;
-												else if (c >= 10)
+
+		else if (c >= 10)
 			c -= 10;
 
 		else if (c >= 5)
@@ -35,11 +36,14 @@ int main(int args, char *argv[])
 
 		else if (c >= 2)
 			c -= 2;
-												else if (c >= 1)
-													c -= 1;
-												else
+
+		else if (c >= 1)
+			c -= 1;
+
+		else
 			break;
-												coins += 1;
+
+		coins += 1;
 	}
 	printf("%d\n", coins);
 	return (0);
